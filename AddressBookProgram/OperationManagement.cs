f11bb55cb1ec4bf5e16e39c14c39878b38f2f739
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-04 13:24:03 +0530

    [Soubarnika]UC2 Adding New Contact Details

--- AddressBookProgram/OperationManagement.cs
+++ AddressBookProgram/OperationManagement.cs
@@ -13,8 +13,8 @@ namespace AddressBookProgram
 
             AddressBookCompute addressBookCompute = new AddressBookCompute();
             ////create object for the class address compute and call the set contact detail method
-            addressBookCompute.AddContactDetails("Jeba", "Kani", "Jeba cottage,bommi nagar", "madurai", "Tamil Nadu", 625107, 8680018236);
-            addressBookCompute.AddContactDetails("jasmine", "Rita", "K.K.Nagar", "chennai", "Tamil Nadu", 600008, 9487812639);
+            addressBookCompute.AddContactDetails("sou", "muthu", "Anna nagar", "madurai", "Tamil Nadu", 625107, 9964747576);
+            addressBookCompute.AddContactDetails("gemini", "muthu", "K.K.Nagar", "chennai", "Tamil Nadu", 600008, 9487812639);
             addressBookCompute.DisplayContact();
         }
     }
